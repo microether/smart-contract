3ebfb35b1832c74a5006ec6d1a68c283351109e7
Mikhail Azaryan <mikhailazaryan1514@gmail.com>
2023-08-12 13:09:54 +0200

    Add main structure

--- smartcontract.sol
+++ smartcontract.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 import "https://github.com/molecule-protocol/molecule-core/blob/main/src/v2/interfaces/IMoleculeController.sol";
 import "./MyCustomController.sol";
-contract MyContract {
+contract Contract {
     MyCustomController public myCustomController;
     IMoleculeController public moleculeGeneralSanctionController;
     uint[] public regionalIds;
@@ -17,4 +17,63 @@ contract MyContract {
         require(myCustomController.check(msg.sender), "Address is not authorized.");
         // Access allowed to function code here
     }
+}
+
+contract MicroEtherContract {
+    address public owner;
+    mapping(address => uint256) public userCreditScores;
+
+    constructor() {
+        owner = msg.sender;
+        userCreditScores = 0;
+    }
+    modifier hasSufficientCredit(uint256 requiredScore) {
+        require(userCreditScores[msg.sender] >= requiredScore, "Insufficient credit score");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    function updateUserCreditScore(address user, uint256 newScore) external {
+        require(msg.sender == owner, "Only the owner can update credit scores");
+        userCreditScores[user] = newScore;
+    }
+
+    function performCreditCheckedAction(uint256 requiredScore) external hasSufficientCredit(requiredScore) {
+        // Only users with sufficient credit score can execute this function
+        // Perform the desired action here
+    }
+
+    function getCreditScore(address user) external view returns (uint256) {
+        return userCreditScores[user];
+    }
+
+    function checkCreditEligibility(address user, uint256 requiredScore) external view returns (bool) {
+        return userCreditScores[user] >= requiredScore;
+    }
+
+struct Owner {
+    address payable ownerAddress;
+}
+
+struct Borrower {
+    address payable borrowerAddress;
+    uint loanAmount;
+    uint groupID;
+    uint dueDate;
+    bool paymentMethod; //true = once a month, false = in the end of duedate
+}
+
+struct Group {
+    uint totalLoanAmount;
+    uint membersCount;
+    bool isPaying;
+    Borrower[] members;
+}
+
+
+
 }
